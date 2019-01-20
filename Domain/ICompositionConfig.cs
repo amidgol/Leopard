b20e9e761073effced2c88dbc19b385922481f3d
Amid Golmohammadi <golmohammadi.edu@gmail.com>
2019-01-21 02:18:26 +0330

    Add some interfaces

---
+++ Domain/ICompositionConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public interface ICompositionConfig
+    {
+         int Tasks { get; set; }
+         int CandidatesPerTask { get; set; }
+
+
+    }
+}
